f4c90e04e850c4e0f5eb19ee15dc8a27cad3ef1c
JoaoTeixeira13 <j.teixeira.dev@gmail.com>
2024-03-24 10:17:35 +0100

    refactor folder structure and remove json explorer functionality from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,10 @@
-import { useEffect, useState } from "react";
 import "./App.css";
-import { JsonExplorer, KeyValuePair } from "./components/json-explorer";
-import demoData from "./test-data/demo-data.json";
-//import secondDemoData from "./test-data/second-demo-data.json";
-import { readJsonProperties } from "./utils/read-json-properties";
-import { initialSearchValue, rootPath, undefinedDisplay } from "./constants";
+import { JsonChallenge } from "./json-challenge/components/json-challenge";
 
 function App() {
-    //to experiment with different data types we can initialize our json here
-    //for an alternativite test case initialize the json as 'secondDemoData', further indention, data types, etc
-
-    const [json] = useState<KeyValuePair>(demoData);
-
-    const [selectedProperty, setSelectedProperty] = useState<string>("");
-
-    const [selectedPath, setSelectedPath] = useState<string>("");
-    const [searchValue, setSearchValue] = useState<string>("");
-
-    useEffect(() => {
-        searchValue.length < 4 && setSelectedProperty(undefinedDisplay);
-        searchValue === "" && setSelectedProperty("");
-
-        if (searchValue.startsWith(rootPath)) {
-            const searchPath = searchValue.replace(rootPath, "");
-            const foundResult = readJsonProperties(json, searchPath);
-
-            setSelectedProperty(foundResult ?? undefinedDisplay);
-        }
-    }, [searchValue, json]);
-
-    useEffect(() => {
-        setSearchValue(selectedPath);
-    }, [selectedPath]);
-
     return (
         <>
-            <section className="input-group">
-                <div>
-                    <p>Property</p>
-                    <input
-                        value={searchValue}
-                        onChange={(e) => {
-                            setSelectedPath(e.target.value);
-                            setSearchValue(e.target.value);
-                        }}
-                    />
-                    {<p className="display-result">{selectedProperty}</p>}
-                </div>
-                <div>
-                    <p>Block / Variable</p>
-                    <input placeholder="Variable" /> {" -"}
-                </div>
-            </section>
-
-            <h4>+ Assign to variable</h4>
-            <h4>+ Assign to block</h4>
-
-            <p>Response</p>
-            <div className="explorer-container">
-                <JsonExplorer
-                    json={json}
-                    selectedPath={initialSearchValue}
-                    setSelectedProperty={setSelectedProperty}
-                    setSelectedPath={setSelectedPath}
-                />
-            </div>
+            <JsonChallenge />
         </>
     );
 }
